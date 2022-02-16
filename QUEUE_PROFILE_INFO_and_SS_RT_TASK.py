5d0c06b7d106dc5aa7f7682161123dc8450ecc72
Frank <itrimsl8696@gmail.com>
2022-02-16 18:52:48 +0800

    Update QUEUE_PROFILE_INFO_and_SS_RT_TASK.py

--- QUEUE_PROFILE_INFO_and_SS_RT_TASK.py
+++ QUEUE_PROFILE_INFO_and_SS_RT_TASK.py
@@ -42,21 +42,11 @@ def analysisTextContent(lines: List):
                 event_list.append(temp_line_list[1])
     event_list = list(set(event_list))
     pprint(event_list)
-    return record_list
+    return record_list, event_list
 
 
-def statisticsEveryEventTime(events: List):
-    ss_rt_events = ['EVT_TFU_RA_REQUEST_INDICATION',
-                    'EVT_RGR_LVL1_CONFIG_REQUEST',
-                    'EVT_UDX_STA_PHBT_START_TIMER',
-                    'EVT_TFU_CRC_INDICATION',
-                    'EVT_COMMON_UE_ACK_INDICATION',
-                    'EVT_L1_MSG_INDICATION',
-                    'EVT_CTF_CONFIG_REQUEST',
-                    'EVT_RGR_LVL1_SI_CONFIG_REQUEST',
-                    'EVT_RGR_LVL1_CCCH_DATA_REQUEST',
-                    'EVT_KW_AMUL_STA_PROH_TIMER',
-                    'EVT_RGR_LVL2_CONFIG_REQUEST']
+def statisticsEveryEventTime(events: List, event_names: List):
+    ss_rt_events = event_names
     ss_rt_task = dict([(element, list()) for element in ss_rt_events])
 
     for event in events:
@@ -83,8 +73,8 @@ def plot_hist(data: Dict):
 
 
 if __name__ == '__main__':
-    lines = readProfileFile('no_traffic.txt')
-    record_list = analysisTextContent(lines)
-    ss_rt_task = statisticsEveryEventTime(record_list)
+    lines = readProfileFile('2 core/MobaXterm_10.255.174.35_20220216_163442.txt')
+    record_list, event_names = analysisTextContent(lines)
+    ss_rt_task = statisticsEveryEventTime(record_list, event_names)
     plot_hist(ss_rt_task)
     # pprint(ss_rt_task)
